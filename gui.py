e8587be42fb8f31ff1c4ad785d2242fa4057ab87
pratoshbharani <pratosh.bharanidharan@gmail.com>
2022-12-14 17:07:43 -0500

    Implemented the Edit button

--- gui.py
+++ gui.py
@@ -4,23 +4,41 @@ import PySimpleGUI as sg
 label = sg.Text("Type in  a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
-help(sg.Button)
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
 
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 20))
-# having two squarebrackets is because we  want both elements in the same
-# row. For separate lines, each element will have its own inner list box
+# having two square brackets is because we  want both elements in the same
+# row. For separate rows, each element will have its own inner list box
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(3, values['todos'])
     match event:
         case 'Add':
             todos = functions.get_todos()
             new_todo = values['todo'] + '\n'
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case 'Edit':
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo'] + '\n'
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
+            
         case sg.WIN_CLOSED:
             break
 window.close()
